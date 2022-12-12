15fa871b4d3e03f6aff1fb9bba37675b02a1c85f
Usuario <Usuario@DESKTOP-EOK7RG0>
2022-12-12 14:03:59 -0300

    Icone Chave senha trocada

--- KittensSports/View/TelaCadastroEditUsuario.Designer.cs
+++ KittensSports/View/TelaCadastroEditUsuario.Designer.cs
@@ -38,7 +38,6 @@
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button17 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.ttbSenha = new System.Windows.Forms.TextBox();
             this.guna2Button15 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button14 = new Guna.UI2.WinForms.Guna2Button();
@@ -51,8 +50,8 @@
             this.ttbEmail = new System.Windows.Forms.TextBox();
             this.InicioLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2GradientPanel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVer
@@ -84,13 +83,13 @@
             // guna2GradientPanel2
             // 
             this.guna2GradientPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.guna2GradientPanel2.Controls.Add(this.guna2Button3);
             this.guna2GradientPanel2.Controls.Add(this.guna2Button2);
             this.guna2GradientPanel2.Controls.Add(this.guna2Button1);
             this.guna2GradientPanel2.Controls.Add(this.guna2Button17);
             this.guna2GradientPanel2.Controls.Add(this.btnGravar);
             this.guna2GradientPanel2.Controls.Add(this.guna2HtmlLabel2);
             this.guna2GradientPanel2.Controls.Add(this.btnVer);
-            this.guna2GradientPanel2.Controls.Add(this.pictureBox2);
             this.guna2GradientPanel2.Controls.Add(this.ttbSenha);
             this.guna2GradientPanel2.Controls.Add(this.guna2Button15);
             this.guna2GradientPanel2.Controls.Add(this.guna2Button14);
@@ -182,22 +181,11 @@
             this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2HtmlLabel2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.guna2HtmlLabel2.Location = new System.Drawing.Point(135, 350);
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(135, 384);
             this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
             this.guna2HtmlLabel2.Size = new System.Drawing.Size(45, 22);
             this.guna2HtmlLabel2.TabIndex = 31;
             this.guna2HtmlLabel2.Text = "Senha";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(427, 373);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(35, 33);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 30;
-            this.pictureBox2.TabStop = false;
             // 
             // ttbSenha
             // 
@@ -205,7 +193,7 @@
             this.ttbSenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ttbSenha.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.ttbSenha.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ttbSenha.Location = new System.Drawing.Point(133, 372);
+            this.ttbSenha.Location = new System.Drawing.Point(133, 406);
             this.ttbSenha.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ttbSenha.Name = "ttbSenha";
             this.ttbSenha.PlaceholderText = "Senha {User}";
@@ -223,7 +211,7 @@
             this.guna2Button15.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2Button15.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guna2Button15.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button15.Image")));
-            this.guna2Button15.Location = new System.Drawing.Point(427, 307);
+            this.guna2Button15.Location = new System.Drawing.Point(427, 341);
             this.guna2Button15.Name = "guna2Button15";
             this.guna2Button15.Size = new System.Drawing.Size(34, 32);
             this.guna2Button15.TabIndex = 21;
@@ -238,7 +226,7 @@
             this.guna2Button14.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2Button14.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guna2Button14.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button14.Image")));
-            this.guna2Button14.Location = new System.Drawing.Point(423, 244);
+            this.guna2Button14.Location = new System.Drawing.Point(423, 278);
             this.guna2Button14.Name = "guna2Button14";
             this.guna2Button14.Size = new System.Drawing.Size(37, 29);
             this.guna2Button14.TabIndex = 20;
@@ -253,7 +241,7 @@
             this.guna2Button13.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2Button13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.guna2Button13.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button13.Image")));
-            this.guna2Button13.Location = new System.Drawing.Point(428, 181);
+            this.guna2Button13.Location = new System.Drawing.Point(428, 215);
             this.guna2Button13.Name = "guna2Button13";
             this.guna2Button13.Size = new System.Drawing.Size(33, 29);
             this.guna2Button13.TabIndex = 14;
@@ -263,7 +251,7 @@
             this.guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2HtmlLabel4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.guna2HtmlLabel4.Location = new System.Drawing.Point(133, 155);
+            this.guna2HtmlLabel4.Location = new System.Drawing.Point(133, 189);
             this.guna2HtmlLabel4.Name = "guna2HtmlLabel4";
             this.guna2HtmlLabel4.Size = new System.Drawing.Size(57, 22);
             this.guna2HtmlLabel4.TabIndex = 22;
@@ -274,7 +262,7 @@
             this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2HtmlLabel3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.guna2HtmlLabel3.Location = new System.Drawing.Point(133, 282);
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(133, 316);
             this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
             this.guna2HtmlLabel3.Size = new System.Drawing.Size(47, 22);
             this.guna2HtmlLabel3.TabIndex = 21;
@@ -285,7 +273,7 @@
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.guna2HtmlLabel1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(134, 219);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(134, 253);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
             this.guna2HtmlLabel1.Size = new System.Drawing.Size(46, 22);
             this.guna2HtmlLabel1.TabIndex = 20;
@@ -297,7 +285,7 @@
             this.ttbUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ttbUsername.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.ttbUsername.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ttbUsername.Location = new System.Drawing.Point(133, 178);
+            this.ttbUsername.Location = new System.Drawing.Point(133, 212);
             this.ttbUsername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ttbUsername.Name = "ttbUsername";
             this.ttbUsername.PlaceholderText = "Identificador {User}";
@@ -311,7 +299,7 @@
             this.ttbNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ttbNome.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.ttbNome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ttbNome.Location = new System.Drawing.Point(133, 241);
+            this.ttbNome.Location = new System.Drawing.Point(133, 275);
             this.ttbNome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ttbNome.Name = "ttbNome";
             this.ttbNome.PlaceholderText = "Nome do Usuário";
@@ -325,7 +313,7 @@
             this.ttbEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ttbEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.ttbEmail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ttbEmail.Location = new System.Drawing.Point(133, 306);
+            this.ttbEmail.Location = new System.Drawing.Point(133, 340);
             this.ttbEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ttbEmail.Name = "ttbEmail";
             this.ttbEmail.PlaceholderText = "Usuário@gmail.com";
@@ -353,6 +341,21 @@
             this.guna2Panel1.Size = new System.Drawing.Size(19, 675);
             this.guna2Panel1.TabIndex = 35;
             // 
+            // guna2Button3
+            // 
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.guna2Button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.guna2Button3.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button3.Image")));
+            this.guna2Button3.Location = new System.Drawing.Point(427, 410);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(33, 29);
+            this.guna2Button3.TabIndex = 35;
+            // 
             // TelaCadastroEditUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -371,7 +374,6 @@
             this.Text = "TelaCadastroUsuario";
             this.guna2GradientPanel2.ResumeLayout(false);
             this.guna2GradientPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -392,12 +394,12 @@
         private TextBox ttbSenha;
         private TextBox ttbNome;
         private TextBox ttbEmail;
-        private PictureBox pictureBox2;
         public Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
         private Guna.UI2.WinForms.Guna2Button guna2Button17;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         public Guna.UI2.WinForms.Guna2HtmlLabel InicioLabel1;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
     }
 }
